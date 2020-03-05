1c2d8543777ee3d6826637dcb6518c3c4255986a
mjamilao <markjasonamilao@gmail.com>
2020-03-05 10:41:09 +0800

    adds new path to book resource

--- app/Book.php
+++ app/Book.php
@@ -3,9 +3,18 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
 
 class Book extends Model
 {
     //turning off the mass assignment protection. explicit the fields your passing in to avoid injection
     protected $guarded = [];
+
+    public function path()
+    {
+        return '/books/'.$this->id;
+
+        //if you decided to have a slug /books/1-Maverick
+        //return '/books/' . $this->id . '-' . Str::slug($this->title);
+    }
 }
